4bb7b827e91d83bcb1e2e90061b5e3ebd1534e56
flogvit <Vegard.Hanssen@menneske.no>
2015-11-24 12:38:33 +0100

    Fixed license in files

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
  *
  * @copyright Cellar Labs AS, 2015, www.cellarlabs.com, all rights reserved
  * @file
- * @license GPL-3.0
+ * @license MIT
  * @author Vegard Hanssen <Vegard.Hanssen@cellarlabs.com>
  *
  */
